48de241fb7fb0742034383b00d13cc4e2a3a5429
Jonathan <jonathan.comberbach@gmail.com>
2018-08-24 22:56:32 +0000

    test components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar.jsx'
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <NavBar />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, click here.
         </p>
       </div>
     );
---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import App from './App.jsx';
+
+class NavBar extends Component {
+  render() {
+    return (
+        <nav>
+    <span> Current Users</span>
+  </nav>
+    );
+  }
+}
+
+export default NavBar;
