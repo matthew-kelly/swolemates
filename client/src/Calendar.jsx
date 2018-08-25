c00b8d8a39f351dff4253a2215592c1f663f1191
Jonathan <jonathan.comberbach@gmail.com>
2018-08-25 18:07:12 +0000

    add calendar

--- client/src/Calendar.jsx
+++ client/src/Calendar.jsx
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
+import { render } from 'react-dom';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
+
+const today = new Date();
+const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
 
 class Calendar extends Component {
   render() {
     return (
-      <div> calendar goes here </div>
+      <InfiniteCalendar
+    width={1355}
+    height={630}
+    selected={false}
+    disabledDays={[]}
+    minDate={2018, 0, 1}
+    min={new Date(2018, 0, 1)}
+    max={new Date(2019, 0, 1)}
+    displayOptions={{
+    layout: 'landscape',
+    showOverlay: false,
+    shouldHeaderAnimate: false
+  }}
+  />
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 require('./styles/styles.css');
 
 
