f7bf7f4b17575702aeb9afeec05288d03a064daf
Jonathan <jonathan.comberbach@gmail.com>
2018-08-25 17:30:44 +0000

    fix mistake with knex

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser')
+const knex = require("knex");
 const database = require("./database")(knex);
 
 app.use(bodyParser.json())
