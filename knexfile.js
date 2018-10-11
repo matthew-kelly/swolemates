55e57c1da8bd8b9bbf6f9cec2d7cf51adfa594a1
Matt Kelly <bmatthewkelly@gmail.com>
2018-10-11 00:57:46 +0000

    create db on heroku

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'final_project',
+      database: 'postgres://zaafqrkhrpgvkk:0d67ae50e3d9f49687c40b9c538f8687e17d3e2483e758f4fb1432993d3729f3@ec2-107-21-233-72.compute-1.amazonaws.com:5432/d3eac0gbh1gtg7',
       user:     'final',
       password: 'final'
     },
@@ -28,7 +28,11 @@ module.exports = {
       max: 10
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 
